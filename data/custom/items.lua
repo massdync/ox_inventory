14920e025fce598435ed1835a26895009adce6e2
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2025-05-18 17:47:12 +0800

    Add new item for debugging

--- data/custom/items.lua
+++ data/custom/items.lua
@@ -13,5 +13,23 @@ return {
 		server = {
 			--export = 'ct.cartracker'
 		}
-	}
+	},
+
+	-- For debugging
+	['saratoga_water'] = {
+		label = 'Saratoga Water',
+		weight = 10,
+		client = {
+			status = {
+				thirst = 1000000,
+				hunger = 1000000,
+				stress = -1000000,
+			},
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'You drank some refreshing water'
+		}
+	},
 }
