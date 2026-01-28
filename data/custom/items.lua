f3e649be89f6413a040bb3e6ef639e939611a21b
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2026-01-28 16:10:44 +0800

    Added items to both items.lua

--- data/custom/items.lua
+++ data/custom/items.lua
@@ -1,3 +1,40 @@
 return {
+    -- [[ Thomas police badge ]] --
+    ['hkp'] = {
+        label = '警察證件',
+        weight = 500,
+        stack = false,         -- Police ID should usually be unique per player, not stackable
+        close = true,          -- Matches shouldClose = true (closes inventory on use)
+        description = 'HKP的證件',
+        client = {
+            image = 'hkp.png',
+            -- Optional: If using this item triggers something (e.g., show ID animation/menu),
+            -- add an export here instead of old QB events
+            -- export = 'your_police_resource.showHKPID'
+        }
+    },
 
+    -- [[ drugseed TEST items ]] --
+    ['weedseed'] = {
+        label = '大麻種子 (測試)',
+        weight = 1,
+    },
+    ['cocaseed'] = {
+        label = '可卡種子 (測試)',
+        weight = 1,
+    },
+    ['iodine'] = {
+        label = '碘 (測試)',
+        weight = 1,
+    },
+    ['acetone'] = {
+        label = '丙酮 (測試)',
+        weight = 1,
+    },
+
+    -- [[ Card test ]] --
+    ['jj_s66_001ssp'] = {
+        label = 'test',
+        weight=1
+    }
 }
