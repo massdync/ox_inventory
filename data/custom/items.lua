697e37cb31894034e59c2180b6d1f29072b9d863
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2026-02-23 16:52:58 +0800

    Added saratoga_water item

--- data/custom/items.lua
+++ data/custom/items.lua
@@ -28,4 +28,24 @@ return {
         label = '丙酮 (測試)',
         weight = 1,
     },
+
+	-- For debugging
+	['saratoga_water'] = {
+		label = 'Saratoga Water',
+        description = "Ashton Hall is proud of you",
+		weight = 10,
+		client = {
+            image = "custom/saratoga_water.png",
+			status = {
+				thirst = 1000000,
+				hunger = 1000000,
+				stress = -1000000,
+			},
+			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+			prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
+			usetime = 2500,
+			cancel = true,
+			notification = 'You drank some refreshing water'
+		}
+	},
 }
